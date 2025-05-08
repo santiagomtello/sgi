9550fe823deb3bdc058218c9275767998b35ca63
santiagomtello <180667150+santiagomtello@users.noreply.github.com>
2025-05-08 07:39:36 -0500

    feat: :sparkles: creada la conexion con la DB

---
+++ infrastucture/mysql/ConnDbFactory.cs
@@ -0,0 +1,14 @@
+using sgi_app.domain.factory;
+
+namespace sgi_app.infrastructure.mysql;
+
+public class ConnDbFactory : IDbFactory
+{
+    private readonly string _connectionString;
+
+    public ConnDbFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+}   
